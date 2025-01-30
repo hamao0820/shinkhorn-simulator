2ca75ac77e20cef28c50faf8cada860767311801
浜田　俊祐 <hamashun0820@icloud.com>
2025-01-30 20:56:35 +0900

    csvを読み込めるようにした

--- index.ts
+++ index.ts
@@ -1,1 +1,52 @@
-console.log("Hello via Bun!");
+const main = () => {
+  console.log("Hello World");
+  const $inputFile = mustQuerySelector<HTMLInputElement>("#input-file");
+  const $buttonRead = mustQuerySelector<HTMLButtonElement>("#button-read");
+
+  $buttonRead.addEventListener("click", async (e) => {
+    const files = $inputFile.files;
+    if (!files) {
+      return;
+    }
+    if (files.length === 0) {
+      return;
+    }
+    const csv = await fetchFile(files[0]);
+    const data = parseCSV(csv);
+    console.log(data);
+  });
+};
+
+const mustQuerySelector = <T extends HTMLElement>(selector: string): T => {
+  const element = document.querySelector(selector);
+  if (!element) {
+    throw new Error(`Element not found: ${selector}`);
+  }
+  return element as T;
+};
+
+const parseCSV = (csv: string): string[][] => {
+  return csv
+    .trim()
+    .split("\n")
+    .map((line) => line.split(",").map((value) => value.trim()));
+};
+
+const fetchFile = async (file: File): Promise<string> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (!e.target) {
+        return;
+      }
+      const result = e.target.result;
+      if (typeof result !== "string") {
+        return;
+      }
+      resolve(result);
+    };
+    reader.readAsText(file);
+  });
+};
+
+main();
